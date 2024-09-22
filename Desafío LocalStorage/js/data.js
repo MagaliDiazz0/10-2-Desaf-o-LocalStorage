a654bbabfc167b9e5e8a8410d5d0674de0c39172
Magalí Díaz <yajaira281096@gmail.com>
2024-09-22 19:18:27 -0300

    agregue comentarios para entender el codigo

--- Desafío LocalStorage/js/data.js
+++ Desafío LocalStorage/js/data.js
@@ -1,2 +1,5 @@
-const storedData = localStorage.getItem('text');
-document.getElementById('data').textContent = storedData ? storedData : 'No hay datos guardados.';
+const storedData = localStorage.getItem('text');    // Obtiene el valor almacenado en localStorage bajo la clave "text"
+
+// Actualiza el contenido del elemento con id "data"
+// Si hay datos almacenados, mostrarlos; de lo contrario, mostrar un mensaje alternativo
+document.getElementById('data').textContent = storedData ? storedData : 'No hay datos guardados.';  
--- Desafío LocalStorage/js/index.js
+++ Desafío LocalStorage/js/index.js
@@ -1,6 +1,6 @@
 document.getElementById('buttonText').addEventListener('click', function() {
-    const inputText = document.getElementById('inputText').value;
-    localStorage.setItem('text', inputText);
-    document.getElementById('inputText').value = '';
+    const inputText = document.getElementById('inputText').value;   // Obtiene el valor del campo de entrada con id 'inputText'
+    localStorage.setItem('text', inputText);                       // Guarda el valor en localStorage bajo la clave 'text'
+    document.getElementById('inputText').value = '';              // Limpia el campo de entrada después de guardar el dato
 });
 
